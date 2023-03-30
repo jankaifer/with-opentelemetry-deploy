757608867b31288dad9563a1b270c1fa423989ff
Jan Kaifer <jan@kaifer.cz>
2023-03-30 18:16:12 +0200

    migrate back to @vercel/otel

--- instrumentation.node.ts
+++ instrumentation.node.ts
@@ -1,19 +1,3 @@
-import { Resource } from "@opentelemetry/resources";
-import { SemanticResourceAttributes } from "@opentelemetry/semantic-conventions";
-import { NodeTracerProvider } from "@opentelemetry/sdk-trace-node";
-import {
-  ConsoleSpanExporter,
-  SimpleSpanProcessor,
-} from "@opentelemetry/sdk-trace-base";
-import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
+import { registerOTel } from "@vercel/otel";
 
-const provider = new NodeTracerProvider({
-  resource: new Resource({
-    [SemanticResourceAttributes.SERVICE_NAME]: "with-opentelemetry-deploy",
-  }),
-});
-
-provider.addSpanProcessor(new SimpleSpanProcessor(new OTLPTraceExporter()));
-provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
-
-provider.register();
+registerOTel("with-opentelemetry-deploy");
