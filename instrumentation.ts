c94071466a25ab2171e7ddc7c64d1fa8b22b4e11
Jan Kaifer <jan@kaifer.cz>
2023-04-17 15:57:09 +0200

    update app name

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,6 +1,6 @@
 import { registerOTel } from "@vercel/otel";
 
 export function register() {
-  registerOTel("with-opentelemetry-deploy");
+  registerOTel("with-opentelemetry-deploy-1");
   globalThis.message = "Hello from instrumentation.ts";
 }
