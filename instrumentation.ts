03b23151887e94a5c748b3bcba2eccc23b8bbcc7
Jan Kaifer <jan@kaifer.cz>
2023-04-27 18:28:37 +0200

    try new version

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,6 +1,6 @@
 import { registerOTel } from "@vercel/otel";
 
 export function register() {
-  registerOTel("with-opentelemetry-deploy-2");
+  registerOTel("with-opentelemetry-deploy-3");
   globalThis.message = "Hello from instrumentation.ts";
 }
